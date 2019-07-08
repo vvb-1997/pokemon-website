1f1b500fb41543f1aedc9cade936988088966c95
Vinay Bhat <vinayvamanbhat@gmail.com>
2019-07-08 15:39:30 +0530

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -9,7 +9,7 @@ function scores(power) {
 Object.keys(data).forEach(function(k) {
     var power = data[k].base;
 
-    html_out += "<div class='container'> <p class='name'>Name:</p>"+ data[k].name.english + "<br>" + data[k].type + "<br>" + scores(power) + "<button class = 'btn-success'>Learn More</button> </div>";
+    html_out += "<div class='container'> <p class='name'>Name: "+ data[k].name.english + "</p><p>Attack Type: " + data[k].type + "</p>Power: " + scores(power) + "</p><button class = 'btn-success'>Learn More</button> </div>";
 });
 console.log(html_out);
 $(".out").append(html_out);
