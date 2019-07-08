09a42c99ba060e3a3cd75654bc75b8a4959fcc74
Vinay Bhat <vinayvamanbhat@gmail.com>
2019-07-08 16:28:26 +0530

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,15 +1,24 @@
 // var url = "https://raw.githubusercontent.com/VVB2/pokemon.json/master/pokedex.json";
 var url = "https://github.com/VVB2/pokemon.json/blob/master/pokedex.json";
 
-var html_out = " ";
+var html_out = "<div class='container-fluid'>";
 function scores(power) {
     return power.Attack;
   }
 Object.keys(data).forEach(function(k) {
     var power = data[k].base;
-
+    if(k%3==0){
+      html_out += "<div class='row'>";
+    }
+    html_out+="<div class='col-sm-3'> <p>Name: "+
+    data[k].name.english + "</p><p>Attack Type: " +data[k].type + "</p><p>Score: "+ scores(power) +
+    "</p><button class = 'btn btn-success'> More</button> </div>";
+    if(k%3==2){
+      html_out+="</div>"
+    }
 });
-$(".out").append(html_out);
+console.log(html_out);
+$(".out").append(html_out+"</div>");
 // async function getData() {
 //   const response = await fetch(url);
 //   const data = await response.json();
