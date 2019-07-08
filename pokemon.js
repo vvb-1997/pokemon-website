1b06d14415cae8213b9ca26943c2eeb2d755566f
Vinay Bhat <vinayvamanbhat@gmail.com>
2019-07-08 12:30:17 +0530

    added the files required to get data
    handled the dependencies on github by downloading the files.

--- pokemon.js
+++ pokemon.js
@@ -1,23 +1,38 @@
-var url = "https://raw.githubusercontent.com/VVB2/pokemon.json/master/pokedex.json";
-async function getData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  //    console.log(data);
-  //    var output=$(".out");
-  var html_out = " ";
+// var url = "https://raw.githubusercontent.com/VVB2/pokemon.json/master/pokedex.json";
+var url = "https://github.com/VVB2/pokemon.json/blob/master/pokedex.json";
 
-  function scores(power) {
+console.log(data);
+var html_out = " ";
+function scores(power) {
     return power.Attack;
   }
-  Object.keys(data).forEach(function(k) {
-    //    console.log(data[k]);
+Object.keys(data).forEach(function(k) {
     var power = data[k].base;
-    console.log(power);
+
     html_out += "<div class='container'> <p class='name'>Name:</p>"+ data[k].name.english + "<br>" + data[k].type + "<br>" + scores(power) + "<button class = 'btn-success'>Learn More</button> </div>";
-  });
+});
+console.log(html_out);
+$(".out").append(html_out);
 
-  $(".out").append(html_out);
-}
-
-getData();
+// async function getData() {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   //    console.log(data);
+//   //    var output=$(".out");
+//   var html_out = " ";
+//
+//   function scores(power) {
+//     return power.Attack;
+//   }
+//   Object.keys(data).forEach(function(k) {
+//     //    console.log(data[k]);
+//     var power = data[k].base;
+//     console.log(power);
+//     html_out += "<div class='container'> <p class='name'>Name:</p>"+ data[k].name.english + "<br>" + data[k].type + "<br>" + scores(power) + "<button class = 'btn-success'>Learn More</button> </div>";
+//   });
+//
+//   $(".out").append(html_out);
+// }
+//
+// getData();
 //console.log(pokemon);
